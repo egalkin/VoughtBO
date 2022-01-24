2be01ea06b95b56943be76df8c118eda1ad43f37
egalkin <89681938355GlebEgorifiolile>
2022-01-25 00:00:23 +0300

    Update project version and create helios deployment script

---
+++ deploy_helios.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mvn clean
+
+mvn package -e -DskipTests
+
+scp -P 2222 -i ~/.ssh/id_rsa \
+    target/MPI_Project-1.0.0-SNAPSHOT.jar \
+    s223378@se.ifmo.ru:/home/s223378/
+
+ssh -P 2222 -i ~/.ssh/id_rsa s223378@se.ifmo.ru << EOF
+pgrep -u s223378 java | xargs kill -9
+nohup java -jar MPI_Project-1.0.0-SNAPSHOT.jar > log.txt &
+EOF
