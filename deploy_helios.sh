35e9a55420fd96e4c500b3d1ef24eff62510fc1d
egalkin <89681938355GlebEgorifiolile>
2022-01-25 00:23:35 +0300

    Update connection command

--- deploy_helios.sh
+++ deploy_helios.sh
@@ -8,7 +8,7 @@ scp -P 2222 -i ~/.ssh/id_rsa \
     target/Vought-1.0.0-SNAPSHOT.jar \
     s223378@se.ifmo.ru:/home/s223378/
 
-ssh -P 2222 -i ~/.ssh/id_rsa s223378@se.ifmo.ru << EOF
+ssh s223378@se.ifmo.ru -p 2222 -i ~/.ssh/id_rsa << EOF
 pgrep -u s223378 java | xargs kill -9
 nohup java -jar Vought-1.0.0-SNAPSHOT.jar > log.txt &
 EOF
